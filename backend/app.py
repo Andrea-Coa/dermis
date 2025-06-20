d5626b0d2c23df1795ad78ba0b9ff4b5d7b140ed
camila <129806557+arosteguicp@users.noreply.github.com>
2025-06-20 12:30:20 -0500

    new back with logistic

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import numpy as np
 from PIL import Image
@@ -25,71 +25,6 @@ CORS(app, resources={
         "allow_headers": ["Content-Type"]
     }
 })
-
-
-"""
-db = SQLAlchemy(app)
-
-
-routine_products = db.Table('routine_products',
-    db.Column('routine_id', db.Integer, db.ForeignKey('routine.id'), primary_key=True),
-    db.Column('product_id', db.Integer, db.ForeignKey('product.id'), primary_key=True),
-    db.Column('order', db.Integer)  # Opcional: para mantener el orden de aplicación de productos
-)
-
-class Product(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(80), nullable=False)
-    price = db.Column(db.Float, nullable=False)
-    #tlp
-    function = db.Column(db.String(30))
-
-class Routine(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    # Relación many-to-many con Product
-    products = db.relationship('Product', secondary=routine_products, backref=db.backref('routines', lazy=True), order_by=routine_products.c.order)  # Opcional: ordenar productos
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    age = db.Column(db.Integer, nullable=False)
-    skin_type = db.Column(db.String(50))
-    conditions = db.Column(db.String(200))
-    routines = db.relationship('Routine', backref='user', lazy=True)
-
-
-
-
-
-
-
-
-
-
-#rutitas
-@app.route('/api/register', methods=['POST'])
-def register_user():
-    data = request.get_json()
-    name = data.get('name')
-    age = data.get('age')
-    email = data.get('email')
-    #luego haremos update a skin_type y conditions
-
-    if not all([name, email, age]):
-        return jsonify({"error":"Missing required fields (name, email, age)"}), 400
-    try:
-        new_user=User(name=name, age=int(age), email=email, skin_type=None, conditions=None)
-        db.session.add(new_user)
-        db.session.commit() #importante el commit
-        return jsonify({"status":"success", "user_id":new_user.id, "message":f"User {new_user.name} registered succesfully"}),201
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-
-"""
 
 @app.route('/api/analyze-skin/cnn', methods=['POST'])
 def analyze_skin_cnn():
@@ -122,22 +57,33 @@ def analyze_skin_cnn():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
     
-@app.route('/api/analyze-skin/efficient-net', methods=['POST'])
+@app.route('/api/analyze-skin/logistic_regression_v1', methods=['POST'])
 def analyze_skin_efficientnet():
-    # Verificar si se envió una imagen
     if 'image' not in request.files:
         return jsonify({"error": "No image provided"}), 400
     
     image_file = request.files['image']
-    
-    #corregir logica clase residual. mixted./
     try:
+
+
+        # Transformación con augmentation
+        aug_transform = transforms.Compose([
+            transforms.RandomResizedCrop(224, scale=(0.9, 1.0)),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.1, hue=0.02),
+            transforms.GaussianBlur(kernel_size=(3, 3), sigma=(0.1, 1.0)),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406],
+                                [0.229, 0.224, 0.225])
+        ])
         
         image = Image.open(io.BytesIO(image_file.read()))
         image = image.convert('RGB') 
         image = image.resize((224, 224))
-        to_tensor = transforms.ToTensor()
-        image = to_tensor(image)
+        image = aug_transform(image)
+        #no need more to_tensor = transforms.ToTensor()
+        #image = to_tensor(image)
+        
         #simple_transform = joblib.load('simple_transform.pkl')
         image = image.unsqueeze(0)
         feature_extractor = joblib.load("feature_extractor.pkl")
@@ -147,7 +93,7 @@ def analyze_skin_efficientnet():
 
 
         
-        model_loaded = joblib.load("mlp_v2.pkl")
+        model_loaded = joblib.load("logistic_regression_v1.pkl")
        # x_new = preprocess_image_for_classical_model(image_path)
         y_pred = model_loaded.predict([feats])[0]
         classes = ['papules', 'black_head', 'white_head', 'acne_scar', 'wrinkle', 'stains','freckle', 'nodules', 'dark_circle', 'pustules', 'skinredness','vascular', 'dark_spot', 'eye_bag']
@@ -164,4 +110,4 @@ def analyze_skin_efficientnet():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
